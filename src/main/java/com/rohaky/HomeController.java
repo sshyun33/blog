707583ac724f37c5c9eb9e94f1d03a80fbb832da
sshyun33 <sshyun33@gmail.com>
2017-03-07 17:20:46 +0900

    initial commit

---
+++ src/main/java/com/rohaky/HomeController.java
@@ -0,0 +1,14 @@
+package com.rohaky;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody String home() {
+       return "Hello World";
+    }
+}
